25ff6935672c86e49bbae213551df59924dee788
110360137 <davechou162147@gmail.com>
2023-11-17 13:02:48 +0800

    你真的超級帥

---
+++ src/ch3/ch3.java
@@ -0,0 +1,67 @@
+package ch3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ch3 
+{
+	public static void main(String[] args)
+	{
+		Car[] car1;
+		car1 = new Car[3];
+		
+		for(int i = 0;i < car1.length;i++)
+		{
+			car1[i] = new Car();
+		}
+		car1[0].setCar(1234, 20.5);
+		car1[1].setCar(2345, 30.5);
+		car1[2].setCar(3456, 40.5);
+		
+		for(int i = 0;i < car1.length ;i++)
+		{
+			car1[i].show();
+		}
+	}
+}
+
+class Car
+{
+	int num;
+	double gas;
+	
+	public Car()
+	{
+		num = 0;
+		gas = 0.0;
+		System.out.println("生產了車子");
+	}
+	
+	public void setCar(int n, double g)
+	{
+		num = n;
+		gas = g;
+		System.out.println("將車號設為" + num + "，汽油量設為" +gas);
+	}
+	
+	public void show()
+	{
+		System.out.println("車號是" + num);
+		System.out.println("汽油量是" + gas);
+	}
+}
+/*
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ch3 
+{
+	public static void main(String[] args)throws IOException
+}
+
+BufferedReader br =
+				new BufferedReader(new InputStreamReader(System.in));
+*/
+
