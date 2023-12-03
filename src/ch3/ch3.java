d5ad644100ab0384d45f95b77431435baae5e7a7
110360137 <davechou162147@gmail.com>
2023-12-03 22:22:34 +0800

    也太帥了吧

--- src/ch3/ch3.java
+++ src/ch3/ch3.java
@@ -4,34 +4,50 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ch3 
+public class ch3
 {
 	public static void main(String[] args)
 	{
-		Car[] car1;
-		car1 = new Car[3];
-		
-		for(int i = 0;i < car1.length;i++)
-		{
-			car1[i] = new Car();
-		}
-		car1[0].setCar(1234, 20.5);
-		car1[1].setCar(2345, 30.5);
-		car1[2].setCar(3456, 40.5);
-		
-		for(int i = 0;i < car1.length ;i++)
-		{
-			car1[i].show();
-		}
+		Car car1 = new Car(1234, 20.5);
+		car1.vShow();
+		car1.mShow();
 	}
 }
 
-class Car
+interface iVehicle
 {
-	int num;
-	double gas;
+	void vShow();
+}
+
+interface iMaterial
+{
+	void mShow();
+}
+
+class Car implements iVehicle,iMaterial
+{
+	private int num;
+	private double gas;
 	
-	public Car()
+	public Car(int n, double g)
+	{
+		num = n;
+		gas = g;
+		System.out.println("生產了車號為" + num + "，汽油量為" + gas + "的車子");
+	}
+	
+	public void vShow()
+	{
+		System.out.println("車號是" + num);
+		System.out.println("汽油量是" + gas);
+	}
+	
+	public void mShow()
+	{
+		System.out.println("車子的材質是鐵");
+	}
+}
+	/*public Car()
 	{
 		num = 0;
 		gas = 0.0;
@@ -42,7 +58,7 @@ class Car
 	{
 		num = n;
 		gas = g;
-		System.out.println("將車號設為" + num + "，汽油量設為" +gas);
+		System.out.println("將車號設為" + num + "，汽油量設為" + gas);
 	}
 	
 	public void show()
@@ -51,17 +67,53 @@ class Car
 		System.out.println("汽油量是" + gas);
 	}
 }
-/*
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
-public class ch3 
+class Car extends Vehicle
 {
-	public static void main(String[] args)throws IOException
+	private int num;
+	private double gas;
+	
+	public Car(int n, double  g)
+	{
+		num = n;
+		gas = g;
+		System.out.println("生產了車號為" + num + "，汽油量為" + gas + "的車子");
+	}
+	
+	public void show()
+	{
+		System.out.println("車號是" + num);
+		System.out.println("汽油量是" + gas);
+		System.out.println("速度是" + speed);
+	}
+	/*
+	public void setCourse(int c)
+	{
+		course = c;
+		System.out.println("將賽車編號設為" + course);
+	}
+	
+	public void show()
+	{
+		System.out.println("賽車的車號是" + num);
+		System.out.println("汽油量是" + gas);
+		System.out.println("賽車的編號是" + course);
+	}
+	
 }
 
-BufferedReader br =
-				new BufferedReader(new InputStreamReader(System.in));
-*/
-
+class Plane implements iVehicle
+{
+	private int flight;
+	
+	public Plane(int f)
+	{
+		flight = f;
+		System.out.println("生產了" + flight + "班次的飛機");
+	}
+	
+	public void show()
+	{
+		System.out.println("飛機的班次是" + flight);
+	}
+}*/
